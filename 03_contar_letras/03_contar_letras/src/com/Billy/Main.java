a272c666be84435e9b0712919ed4de45dcdf957b
Unknown <Eric.Mtz.DAM@gmail.com>
2017-11-27 21:30:42 +0100

    Pequeñas mejoras

--- 03_contar_letras/03_contar_letras/src/com/Billy/Main.java
+++ 03_contar_letras/03_contar_letras/src/com/Billy/Main.java
@@ -10,7 +10,7 @@ public class Main {
         char[] abecedario = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 
-        System.out.println("------------------");
+        System.out.println("          ------------------" + "\n" + "                letras      " + "\n" + "          ------------------");
         for (int vueltas = 0; vueltas < 25; vueltas++) {
             if (valorLetras[vueltas] != 0) {
                 System.out.println("Letra: " + abecedario[vueltas] + " | Numero de veces repetida: " + valorLetras[vueltas]);
